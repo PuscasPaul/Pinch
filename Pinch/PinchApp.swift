aad5d4a1f98d0c567c7deb0b7348362b3df18391
Paul <puscasspaul@yahoo.com>
2023-07-25 21:58:47 +0300

    Initial Commit

---
+++ Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by Puscas Paul on 25.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
